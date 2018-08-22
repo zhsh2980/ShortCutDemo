a1f4b5b2fa8f125dd579f50184962ee7cdfa35f9
shanz01 <shanz01@jumei.com>
2018-08-23 01:31:47 +0800

    fix

--- app/src/main/java/shortcut/bro/com/shortcutdemo/BaseActivity.java
+++ app/src/main/java/shortcut/bro/com/shortcutdemo/BaseActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.DisplayCutout;
 import android.view.View;
+import android.view.WindowManager;
 
 import java.util.List;
 
@@ -28,6 +29,7 @@ public abstract class BaseActivity extends AppCompatActivity {
             int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+//                    | View.SYSTEM_UI_FLAG_FULLSCREEN;//隐藏状态栏
             decorView.setSystemUiVisibility(option);
             getWindow().setNavigationBarColor(Color.TRANSPARENT);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
@@ -35,62 +37,34 @@ public abstract class BaseActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 //
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-//            Log.d("BaseActivity", "Build.VERSION.SDK_INT: P");
-//            WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
-//            layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER;
-//            getWindow().setAttributes(layoutParams);
-//        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            Log.d("BaseActivity", "Build.VERSION.SDK_INT: P");
+            WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+            layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            getWindow().setAttributes(layoutParams);
+        }
 
         getNotchParams();
-
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
-//            final View contentView = getWindow().getDecorView();
-//            contentView.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
-//                @RequiresApi(api = 28)
-//                @Override
-//                public WindowInsets onApplyWindowInsets(View view, WindowInsets insets) {
-//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-//                        DisplayCutout cutout = insets.getDisplayCutout();
-//                        Log.e(TAG, "cutout == null" );
-//                        if (cutout != null) {
-//                            List<Rect> rects = cutout.getBoundingRects();
-//                            if (rects == null || rects.size() == 0) {
-//                                Log.e(TAG, "rects == null || rects.size() == 0, is not notch screen");
-//                            } else {
-//                                Log.e(TAG, "rect size:" + rects.size());
-//                                contentView.setPadding(cutout.getSafeInsetLeft(), cutout.getSafeInsetTop(), cutout.getSafeInsetRight(),
-//                                        cutout.getSafeInsetBottom());
-//                            }
-//                        }
-//                    }
-//                    return insets;
-//                }
-//            });
-//        }
-
-//
-//        View decorView = getWindow().getDecorView();
-//        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-//        decorView.setSystemUiVisibility(uiOptions);
 
     }
 
     @TargetApi(28)
     public void getNotchParams() {
         final View decorView = getWindow().getDecorView();
-
         decorView.post(new Runnable() {
             @Override
             public void run() {
                 DisplayCutout displayCutout = decorView.getRootWindowInsets().getDisplayCutout();
+                if (displayCutout == null){
+                    return;
+                }
                 Log.e("TAG", "安全区域距离屏幕左边的距离 SafeInsetLeft:" + displayCutout.getSafeInsetLeft());
                 Log.e("TAG", "安全区域距离屏幕右部的距离 SafeInsetRight:" + displayCutout.getSafeInsetRight());
                 Log.e("TAG", "安全区域距离屏幕顶部的距离 SafeInsetTop:" + displayCutout.getSafeInsetTop());
                 getDisplayCutoutHeight(displayCutout.getSafeInsetTop());
                 Log.e("TAG", "安全区域距离屏幕底部的距离 SafeInsetBottom:" + displayCutout.getSafeInsetBottom());
-                decorView.setPadding(displayCutout.getSafeInsetLeft(), displayCutout.getSafeInsetTop(), displayCutout.getSafeInsetRight(),
-                        displayCutout.getSafeInsetBottom());
+//                decorView.setPadding(displayCutout.getSafeInsetLeft(), displayCutout.getSafeInsetTop(), displayCutout.getSafeInsetRight(),
+//                        displayCutout.getSafeInsetBottom());
                 List<Rect> rects = displayCutout.getBoundingRects();
                 if (rects == null || rects.size() == 0) {
                     Log.e("TAG", "不是刘海屏");
--- app/src/main/java/shortcut/bro/com/shortcutdemo/MainActivity.java
+++ app/src/main/java/shortcut/bro/com/shortcutdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends BaseActivity {
     Button btnTop;
@@ -12,14 +13,10 @@ public class MainActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-////            Log.d("BaseActivity", "Build.VERSION.SDK_INT: P");
-//            WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
-//            layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT;
-//            getWindow().setAttributes(layoutParams);
-//        }
+
 //        ActionBar actionBar = getSupportActionBar();
 //        actionBar.hide();
+
         setContentView(R.layout.activity_main);
 
 
@@ -34,9 +31,9 @@ public class MainActivity extends BaseActivity {
     @Override
     void getDisplayCutoutHeight(int height) {
         Log.e("TAG", "安全区域距离屏幕上边的距离 :" + height);
-//        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(ll_bar.getLayoutParams());
-////        lp.setMargins(0, SizeUtils.px2dp(height), 0, 0);
-//        lp.setMargins(0, height, 0, 0);
-//        ll_bar.setLayoutParams(lp);
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(ll_bar.getLayoutParams());
+//        lp.setMargins(0, SizeUtils.px2dp(height), 0, 0);
+        lp.setMargins(0, height, 0, 0);
+        ll_bar.setLayoutParams(lp);
     }
 }
